95f366db300d389c3e2c1f6d53532d8d46694d40
jackdemarinis <123496770+jackdemarinis@users.noreply.github.com>
2026-01-22 22:13:39 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: 'all'
+  },
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['.railway.app', 'localhost']
+    allowedHosts: 'all'
   }
 })
